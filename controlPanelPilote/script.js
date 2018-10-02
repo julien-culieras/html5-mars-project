3ecd1504e5ae8ab43c3d52c7ecf3d292ae1dc6ec
Julien CULIERAS <julien.culieras@gmail.com>
2018-10-02 14:11:57 +0200

    rotation ok

--- controlPanelPilote/script.js
+++ controlPanelPilote/script.js
@@ -5,8 +5,10 @@ window.onload = function () {
      */
     let puissancePropulseur = 0.5;
     let interval = null;
-    let angleRotation = 180;
+    let angleRotation = 0;
     const timeMove = 100;
+    const pasAngleRotation = 15;
+    const angleBase = 45;
 
     /*
      Elements DOM
@@ -14,6 +16,12 @@ window.onload = function () {
     const btnAvancer = document.getElementById('btnAvancer');
     const affichagePropulseur = document.getElementById('affichagePropulseur');
     const rangePropulseur = document.getElementById('rangePropulseur');
+    const btnRotateUp = document.getElementById('btnRotateUp');
+    const btnRotateDown = document.getElementById('btnRotateDown');
+    const rocket = document.getElementById('rocketIcon');
+
+    let oldRotate = rocket.style;
+    console.log(oldRotate);
 
     changePuissancePropulseur();
 
@@ -21,16 +29,36 @@ window.onload = function () {
     btnAvancer.addEventListener('mouseup', stop);
     rangePropulseur.addEventListener('change', changePuissancePropulseur);
 
+    btnRotateUp.addEventListener('click', rotateUp);
+    btnRotateDown.addEventListener('click', rotateDown);
+
+
+    document.addEventListener('keydown', (event) => {
+        if (event.key === 'ArrowRight') avancer();
+        else if (event.key === 'ArrowUp') rotateUp();
+        if (event.key === 'ArrowDown') rotateDown();
+    });
+
+    document.addEventListener('keyup', (event) => {
+        if (event.key === 'ArrowRight') stop();
+    });
+
 
     function avancer() {
-        interval = setInterval(function () {
-            console.log('Avance 100ms');
-        }, timeMove)
+        if (!interval){
+            interval = setInterval(function () {
+                console.log('Avance 100ms');
+                /*
+                spaceship:move(timeMove, puissancePropulseur);
+                */
+            }, timeMove)
+        }
     }
 
 
     function stop() {
         clearInterval(interval);
+        interval = null;
         console.log('Le vaisseaux s\'arrete');
     }
 
@@ -38,6 +66,43 @@ window.onload = function () {
         affichagePropulseur.innerText = rangePropulseur.value + ' / ' + rangePropulseur.getAttribute('max');
         puissancePropulseur = rangePropulseur.value/rangePropulseur.getAttribute('max');
         console.log(puissancePropulseur)
+    }
+
+    function rotateUp() {
+        rotate('down');
+    }
+
+    function rotateDown() {
+        rotate('up');
+    }
+
+    function rotate(type) {
+        let oldRotate = rocket.style.transform;
+        let newRotate = null;
+        let angle = null;
+
+        if (!oldRotate) oldRotate = angleBase;
+        else oldRotate = oldRotate.substring(oldRotate.indexOf("(") + 1, oldRotate.indexOf("deg"));
+
+        oldRotate = Number(oldRotate);
+
+        if (type === 'down'){
+            angle = oldRotate - pasAngleRotation;
+            newRotate = "rotate(" + angle + "deg)";
+            rocket.style.transform = newRotate;
+        }
+        else{
+            angle = oldRotate + pasAngleRotation;
+            newRotate = "rotate(" + angle + "deg)";
+            rocket.style.transform = newRotate;
+        }
+
+        angleRotation = angle - 45;
+
+        /*
+          spaceship:turnto(angleRotation);
+         */
+
     }
 };
 
